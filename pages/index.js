9a62ae1a34ee3d95e6cf1b32c489fc21effd5b53
Taibu Haroub <haroubtaibu@gmail.com>
2022-01-19 01:10:59 +0300

    Complete App

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from "next/head";
 
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -18,6 +20,27 @@ export default function Home() {
       <Nav />
 
       {/* Results  */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  );
+  // const request = await fetch(
+  //   `https://api.themoviedb.org/3/movie/550?api_key=641b8592460aebd14f7048b49b60ba9c`
+  // );
+  const response = await request.json();
+
+  return {
+    props: {
+      results: response.results,
+    },
+  };
 }
